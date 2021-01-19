e1b6ead737241984642d267a99123e73503efcbb
jorgadev <radovanjorgic14@gmail.com>
2021-01-19 12:06:22 +0100

    Finished in my way

--- app.js
+++ app.js
@@ -1,35 +1,101 @@
-// Function for fetching data from json file
+// Main logic of program
+fetchData().then((data) => {
+  checkIfArrayOrObject(data);
+  displayData();
+  clickLogic();
+});
+
+// Fetch from json and store in data variable
 async function fetchData() {
   const response = await fetch("data.json");
   const data = response.json();
   return data;
 }
 
-// Fetching data
-fetchData().then((data) => {
-  checkIfArrayOrObject(data);
-  displayData();
-});
-
-// Array for key value pairs of elements
+// Array for storing objects and their main properties
 let rows = [];
 
-// Function for checking is passed data array or object
+// Recursion function to get array of objects with id, name, parent_id and hasChildren properties
 function checkIfArrayOrObject(data) {
-  // Array
+  // Check if array
   if (!!data && data.constructor === Array) {
     data.forEach((element) => checkIfArrayOrObject(element));
   }
-  // Object
+  // Check if object
   else {
-    rows.push({ id: data.id, name: data.name, parent_id: data.parent_id });
+    let hasChildren = false;
+    if (data.children.length != 0) {
+      hasChildren = true;
+    }
+    rows.push({
+      id: data.id,
+      name: data.name,
+      parent_id: data.parent_id,
+      hasChildren: hasChildren,
+    });
     if (data.children.length !== 0) {
       checkIfArrayOrObject(data.children);
     }
   }
 }
 
-// Function for displaying data by pairs array
+// Display data
 function displayData() {
-  console.log(rows);
+  rows.forEach((row) => {
+    let ul = document.createElement("ul");
+    if (row.parent_id == 0) {
+      ul.classList.add(`_${row.id}`);
+      ul.innerHTML = `<i class="arrow" style="pointer-events: none; margin-left: -21px">></i> ${row.name}`;
+    } else {
+      if (row.hasChildren) {
+        document.querySelector(
+          `._${row.parent_id}`
+        ).innerHTML += `<ul class="_${row.id} _${row.parent_id}" style="display: none; margin-left: -21px"><i class="arrow" style="pointer-events: none">></i> ${row.name}</ul>`;
+      } else {
+        document.querySelector(
+          `._${row.parent_id}`
+        ).innerHTML += `<li class="_${row.id} _${row.parent_id}" style="cursor: default; display:none;">${row.name}</li>`;
+      }
+    }
+    if (ul.innerHTML != "") {
+      document.querySelector(".container").appendChild(ul);
+    }
+  });
+}
+
+// Click logic, expanding and closing
+function clickLogic() {
+  document.querySelector(".container").addEventListener("click", (e) => {
+    let children = Array.from(e.target.children).slice(
+      1,
+      e.target.children.length
+    );
+    if (e.target.children.length > 0) {
+      if (e.target.children[0].textContent == ">") {
+        // Expand element
+        e.target.children[0].textContent = "<";
+        children.forEach((child) => {
+          child.style.display = "block";
+          child.parentElement.classList.add("expanded");
+        });
+      } else {
+        closeElement();
+        // Close element
+        function closeElement() {
+          children.forEach((child) => {
+            if (Array.from(child.classList).includes("expanded")) {
+              child.children[0].textContent = ">";
+              children = Array.from(child.children).slice(
+                1,
+                child.children.length
+              );
+              closeElement();
+            }
+            child.style.display = "none";
+            e.target.children[0].textContent = ">";
+          });
+        }
+      }
+    }
+  });
 }
